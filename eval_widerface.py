212f43fc1bf6ad4c136a2759eb680d8fb953d404
Your Name <you@example.com>
2019-11-22 14:28:34 +0700

    update eval scritp

--- eval_widerface.py
+++ eval_widerface.py
@@ -4,7 +4,6 @@ import torch
 import torch.nn as nn
 import os
 from tqdm import tqdm
-from utils import cpu_nms as  nms
 import time
 def box_area(boxes):
     """
@@ -48,39 +47,107 @@ def box_iou(boxes1, boxes2):
 
     iou = inter / (area1[:, None] + area2 - inter)
     return iou
-def get_detections(img_batch, model,score_threshold=0.5, iou_threshold=0.5):
-    model.eval()
-    with torch.no_grad():
-        classifications, bboxes, landmarks = model(img_batch)
-        batch_size = classifications.shape[0]
-        picked_boxes = []
-        picked_landmarks = []
-        
-        for i in range(batch_size):
-            classification = torch.exp(classifications[i,:,:])
-            bbox = bboxes[i,:,:]
-            landmark = landmarks[i,:,:]
 
-            # choose positive and scores > score_threshold
-            scores, argmax = torch.max(classification, dim=1)
-            argmax_indice = argmax==1
-            scores_indice = scores > score_threshold
-            positive_indices = argmax_indice & scores_indice
-            
-            scores = scores[positive_indices]
 
-            if scores.shape[0] == 0:
-                picked_boxes.append(None)
-                picked_landmarks.append(None)
+    def transform(h, w):
+        img_h_new, img_w_new = int(np.ceil(h / 32) * 32), int(np.ceil(w / 32) * 32)
+        scale_h, scale_w = img_h_new / h, img_w_new / w
+        return img_h_new, img_w_new, scale_h, scale_w
+    def nms(boxes, scores, nms_thresh):
+        x1 = boxes[:, 0]
+        y1 = boxes[:, 1]
+        x2 = boxes[:, 2]
+        y2 = boxes[:, 3]
+        areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+        order = np.argsort(scores)[::-1]
+        num_detections = boxes.shape[0]
+        suppressed = np.zeros((num_detections,), dtype=np.bool)
+
+        keep = []
+        for _i in range(num_detections):
+            i = order[_i]
+            if suppressed[i]:
                 continue
+            keep.append(i)
 
-            bbox = bbox[positive_indices]
-            landmark = landmark[positive_indices]
-            keep = nms(bbox, scores, iou_threshold)
-            keep_boxes = bbox[keep]
-            keep_landmarks = landmark[keep]
-            picked_boxes.append(keep_boxes)
-            picked_landmarks.append(keep_landmarks)
+            ix1 = x1[i]
+            iy1 = y1[i]
+            ix2 = x2[i]
+            iy2 = y2[i]
+            iarea = areas[i]
+
+            for _j in range(_i + 1, num_detections):
+                j = order[_j]
+                if suppressed[j]:
+                    continue
+
+                xx1 = max(ix1, x1[j])
+                yy1 = max(iy1, y1[j])
+                xx2 = min(ix2, x2[j])
+                yy2 = min(iy2, y2[j])
+                w = max(0, xx2 - xx1 + 1)
+                h = max(0, yy2 - yy1 + 1)
+
+                inter = w * h
+                ovr = inter / (iarea + areas[j] - inter)
+                if ovr >= nms_thresh:
+                    suppressed[j] = True
+
+        return keep
+    def decode(heatmap, scale, offset, landmark, size, threshold=0.1):
+        
+        heatmap = np.squeeze(heatmap)
+        scale0, scale1 = scale[0, 0, :, :], scale[0, 1, :, :]
+        offset0, offset1 = offset[0, 0, :, :], offset[0, 1, :, :]
+        c0, c1 = np.where(heatmap > threshold)
+    
+        boxes, lms = [], []
+        if len(c0) > 0:
+            for i in range(len(c0)):
+                s0, s1 = np.exp(scale0[c0[i], c1[i]]) * 4, np.exp(scale1[c0[i], c1[i]]) * 4
+                o0, o1 = offset0[c0[i], c1[i]], offset1[c0[i], c1[i]]
+                s = heatmap[c0[i], c1[i]]
+                x1, y1 = max(0, (c1[i] + o1 + 0.5) * 4 - s1 / 2), max(0, (c0[i] + o0 + 0.5) * 4 - s0 / 2)
+                x1, y1 = min(x1, size[1]), min(y1, size[0])
+                boxes.append([x1, y1, min(x1 + s1, size[1]), min(y1 + s0, size[0]), s])
+                lm = []
+                for j in range(5):
+                    lm.append(landmark[0, j * 2 + 1, c0[i], c1[i]] * s1 + x1)
+                    lm.append(landmark[0, j * 2, c0[i], c1[i]] * s0 + y1)
+                lms.append(lm)
+            boxes = np.asarray(boxes, dtype=np.float32)
+            keep = nms(boxes[:, :4], boxes[:, 4], 0.3)
+            boxes = boxes[keep, :]
+            lms = np.asarray(lms, dtype=np.float32)
+            lms = lms[keep, :]
+        return boxes, lms
+
+def get_detections(img_batch, model,score_threshold=0.5, iou_threshold=0.5, cuda = True, threshold=0.5):
+    model.eval()
+    img_h_new, img_w_new, scale_h, scale_w = transform(height, width)
+    with torch.no_grad():
+        imgs = self.transform_img(imgs)
+        imgs = torch.unsqueeze(imgs, 0)
+        picked_boxes, picked_landmarks = [], []
+
+        if cuda:
+            img = img.cuda()
+        outputs = model(img)[0]
+        batch_size = out['hm'].shape[0]
+        for i in range(batch_size):
+            out = outputs[i]
+            heatmap, scale, offset, lms = out['hm'], out['wh'], out['reg'], out['lm']
+            end = datetime.datetime.now()
+            dets, lms = decode(heatmap, scale, offset, lms, (img_h_new, img_w_new), threshold=threshold)
+
+            if len(dets) > 0:
+                dets[:, 0:4:2], dets[:, 1:4:2] = dets[:, 0:4:2] / scale_w, dets[:, 1:4:2] / scale_h
+                lms[:, 0:10:2], lms[:, 1:10:2] = lms[:, 0:10:2] / scale_w, lms[:, 1:10:2] / scale_h
+            else:
+                dets = np.empty(shape=[0, 5], dtype=np.float32)
+                lms = np.empty(shape=[0, 10], dtype=np.float32)
+        picked_boxes.append(keep_boxes)
+        picked_landmarks.append(keep_landmarks)
         
         return picked_boxes, picked_landmarks
 
@@ -103,16 +170,15 @@ def compute_overlap(a,b):
     return torch.from_numpy(intersection / ua)    
 
 
-def evaluate(val_data,retinaFace,threshold=0.5):
+def evaluate(val_data,model,threshold=0.5):
     recall = 0.
     precision = 0.
     #for i, data in tqdm(enumerate(val_data)):
     for data in tqdm(iter(val_data)):
-        img_batch = data['img'].cuda()
-        annots = data['annot'].cuda()
+        img_batch = data['input'].cuda()
+        annots = data['meta']['gt_det'].cuda()
 
-
-        picked_boxes,_ = get_detections(img_batch,retinaFace)
+        picked_boxes,_ = get_detections(img_batch, model)
         recall_iter = 0.
         precision_iter = 0.
 
